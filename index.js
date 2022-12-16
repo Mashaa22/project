b7d73aa3ee1435e2612103a90da2f76e76db94de
AlexMachariaMaranga <amaranga22@gmail.com>
2022-12-16 09:39:35 +0300

    created list for albums

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ function loadAlbums() {
     
     fetch('http://localhost:3000/albums')
     .then(resp => resp.json())
-    .then(data => {
+        .then(data => {
+        
+            (data).forEach(albums => {
+
+                const aTitle = document.createElement('p')
+                const albumList = document.createElement('li')
+                
+                aTitle.innerText = albums.title
+                albumList.appendChild(aTitle)
+                Albums.appendChild(albumList)
+        
+                albumList.addEventListener('click', () =>{
+                    albumSetup.innerHTML = ''
+                    fDetails(albums, albumSetup)
+                })
+            });
+        })
+        }
